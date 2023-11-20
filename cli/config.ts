042d7620b78f9cfb2b021f13fca0922d256d2375
Mihail Mojsoski <mihail@mojsoski.mk>
2023-11-20 21:51:09 +0100

    added command line flag parser

--- cli/config.ts
+++ cli/config.ts
@@ -3,6 +3,7 @@ import log from "./log";
 import { open } from "fs/promises";
 import { blockFromSubject } from "@mojsoski/streams";
 import { ServiceConfig } from "../lib";
+import { flags } from "./flags";
 
 export type Config = {
   path?: string;
@@ -24,6 +25,7 @@ export async function getCurrentConfig() {
   if (cachedConfig) {
     return cachedConfig;
   }
+
   log("debug", `Current config: ${process.cwd()}/services.json`);
   let file = "";
   try {
@@ -45,9 +47,47 @@ export async function getCurrentConfig() {
       log("error", "services.json is not an object");
       throw 1;
     }
+
+    for (const flagKey in flags) {
+      try {
+        const keyParts = flagKey.split(".");
+        let configItem: Record<any, any> = config;
+        for (let part of keyParts.slice(0, keyParts.length - 1)) {
+          try {
+            part = JSON.parse(part);
+          } catch {}
+          if (typeof configItem !== "object") {
+            log(
+              "error",
+              `Cannot override setting of '${flagKey}': got '${typeof configItem}', expected 'object'`
+            );
+            throw 1;
+          }
+          if (typeof configItem[part] === "undefined") {
+            configItem[part] = {};
+          }
+          configItem = configItem[part];
+        }
+
+        let flagValue = flags[flagKey];
+
+        try {
+          flagValue = JSON.parse(flagValue);
+        } catch {}
+
+        log(`debug`, `Overriding '${flagKey}' = '${flagValue}'`);
+        configItem[keyParts[keyParts.length - 1]] = flagValue;
+      } catch (e) {
+        log(`error`, `Overriding '${flagKey}' has failed: ${e}`);
+        throw 1;
+      }
+    }
     cachedConfig = config;
     return config;
-  } catch {
+  } catch (e) {
+    if (typeof e === "number") {
+      throw e;
+    }
     log("error", "Failed parsing services.json");
     throw 1;
   }
